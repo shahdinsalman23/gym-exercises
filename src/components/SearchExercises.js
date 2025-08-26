cab23a4326f7dbd6ed6adae1ca33beb21c1f16d1
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-08-26 22:46:19 +0500

    updated

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,32 +1,32 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
-      const bodyPartList = await ('https://exercisedb.p.rapidapi.com/exercises/targetList', exerciseOptions)
+      const bodyPartList = await fetchData('https://exercisedb.p.rapidapi.com/exercises/targetList', exerciseOptions)
       setBodyParts(['all', ...bodyPartList])
     }
-    fetchExercisesData(); 
+    fetchExercisesData();
   }, [])
-  
 
-  const handleSearch = async () => { 
-    if(search) {
-       const exercisesData =  await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-       
-       const searchedExercises = exercisesData.filter(
-        (exercise) => exercise.name.toLowerCase().includes(search) 
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
           || exercise.target.toLowerCase().includes(search)
           || exercise.equipment.toLowerCase().includes(search)
           || exercise.bodyPart.toLowerCase().includes(search)
-       )
+      )
       setSearch('')
       setExercises(searchedExercises)
     }
@@ -44,7 +44,7 @@ const SearchExercises = () => {
           Search
         </Button>
         <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-
+          <HorizontalScrollbar data={bodyParts} setBodyPart={setBodyPart} bodyPart={bodyPart} />
         </Box>
       </Box>
     </Stack>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,17 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import HeroBanner from '../components/HeroBanner'
 import SearchExercises from './../components/SearchExercises';
 import Exercises from '../components/Exercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
   return (
     <Box>
         <HeroBanner/>
-        <SearchExercises/>
-        <Exercises/>
+        <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+        <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
     </Box>
   )
 }
